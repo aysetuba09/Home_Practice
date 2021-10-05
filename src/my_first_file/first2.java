a090db16ea6985da4499246cbcc05183a09876cd
aysetuba09 <atuba_90@hotmail.com>
2021-10-05 11:25:36 -0600

    practice2 update 2. file

--- src/my_first_file/first2.java
+++ src/my_first_file/first2.java
@@ -3,5 +3,6 @@ package my_first_file;
 public class first2 {
     public static void main(String[] args) {
         System.out.println("I am learning git");
+        // Now I am updating my file
     }
 }
