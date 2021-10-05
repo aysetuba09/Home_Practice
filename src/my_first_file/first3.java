e1ff5669fb2a0e4955e238c0caed201079fd195e
aysetuba09 <atuba_90@hotmail.com>
2021-10-05 11:47:49 -0600

    practice3 update 3. file

--- src/my_first_file/first3.java
+++ src/my_first_file/first3.java
@@ -3,5 +3,6 @@ package my_first_file;
 public class first3 {
     public static void main(String[] args) {
         System.out.println("making another class");
+        // again I am updating to see green line on github.
     }
 }
