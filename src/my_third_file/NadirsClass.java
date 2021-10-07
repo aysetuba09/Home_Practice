0f8689a926b98d689b13efbe3626493d2ccc0a90
aysetuba09 <88247549+aysetuba09@users.noreply.github.com>
2021-10-06 22:29:33 -0600

    Update NadirsClass.java

--- src/my_third_file/NadirsClass.java
+++ src/my_third_file/NadirsClass.java
@@ -3,5 +3,7 @@ package my_third_file;
 public class NadirsClass {
     public static void main(String[] args) {
         System.out.println("Say hello to your soft skill class");
+        // I am editing this file in github
+        // it is really fun:)))
     }
 }
